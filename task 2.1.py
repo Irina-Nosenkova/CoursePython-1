af8b4764e38920b5ffbf3bcf8d99cdd1fd4f1082
Irina-Nosenkova <nosenkova.i.s@gmail.com>
2025-01-15 22:16:27 +0300

    Add files via upload

--- task 2.1.py
+++ task 2.1.py
@@ -6,10 +6,9 @@ increase = 0.05  # Ежемесячный рост цен
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
 months = 0
-while True:
+
+while money_capital >= (spend - salary):
     difference = spend - salary
-    if difference > money_capital:
-        break
     months += 1
     money_capital -= difference
     spend *= 1 + increase
--- task 2.2.py
+++ task 2.2.py
@@ -7,8 +7,7 @@ increase = 0.03  # Ежемесячный рост цен
 
 money_capital = 0
 for month in range(months):
-    if spend > salary:
-        money_capital += (spend - salary)
+    money_capital += (spend - salary)
     spend *= (1 + increase)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
